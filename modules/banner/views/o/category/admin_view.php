06e809f989dc931931139cf6a59d465066867f49
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-25 07:41:44 +0700

    modify manual book (paging)

--- modules/banner/views/o/category/admin_view.php
+++ modules/banner/views/o/category/admin_view.php
@@ -23,17 +23,42 @@
 		'data'=>$model,
 		'attributes'=>array(
 			'cat_id',
-			'publish',
-			'name',
-			'desc',
+			array(
+				'name'=>'publish',
+				'value'=>$model->publish == 1 ? Chtml::image(Yii::app()->theme->baseUrl.'/images/icons/publish.png') : Chtml::image(Yii::app()->theme->baseUrl.'/images/icons/unpublish.png'),
+				'type' => 'raw',
+			),
+			array(
+				'name'=>'name',
+				'value'=>Phrase::trans($model->name),
+			),
+			array(
+				'name'=>'desc',
+				'value'=>Phrase::trans($model->desc),
+			),
 			'cat_code',
-			'banner_size',
-			'banner_limit',
-			'creation_date',
-			'creation_id',
-			'modified_date',
-			'modified_id',
 			'slug',
+			'banner_limit',
+			array(
+				'name'=>'banner_size',
+				'value'=>BannerCategory::getPreviewSize($model->banner_size),
+			),
+			array(
+				'name'=>'creation_date',
+				'value'=>!in_array($model->creation_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00')) ? Utility::dateFormat($model->creation_date, true) : '-',
+			),
+			array(
+				'name'=>'creation_id',
+				'value'=>$model->creation->displayname,
+			),
+			array(
+				'name'=>'modified_date',
+				'value'=>!in_array($model->modified_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00')) ? Utility::dateFormat($model->modified_date, true) : '-',
+			),
+			array(
+				'name'=>'modified_id',
+				'value'=>$model->modified->displayname,
+			),
 		),
 	)); ?>
 </div>
