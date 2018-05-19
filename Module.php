e4b0a08ba65853c61d36039eaef58948562c64bd
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-05-19 09:20:06 +0700

    commit

--- Module.php
+++ Module.php
@@ -1,17 +1,17 @@
 <?php
-namespace ommu\banner;
-
 /**
  * banner module definition class
- * version: 0.0.1
  *
- * @copyright Copyright (c) 2017 ECC UGM (ecc.ft.ugm.ac.id)
- * @link http://ecc.ft.ugm.ac.id
  * @author Putra Sudaryanto <putra@sudaryanto.id>
- * @created date 15 September 2017, 19:05 WIB
  * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2017 ECC UGM (ecc.ft.ugm.ac.id)
+ * @created date 15 September 2017, 19:05 WIB
+ * @link http://ecc.ft.ugm.ac.id
  *
  */
+
+namespace ommu\banner;
+
 class Module extends \app\components\Module
 {
 	public $layout = 'main';
