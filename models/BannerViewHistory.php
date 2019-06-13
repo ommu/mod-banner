1e2b266fc205a0e64942530e35101292b8d472bf
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-06-13 17:15:23 +0700

    add isConsole in models

--- models/BannerViewHistory.php
+++ models/BannerViewHistory.php
@@ -96,6 +96,9 @@ class BannerViewHistory extends \app\components\ActiveRecord
 	{
 		parent::init();
 
+		if(!(Yii::$app instanceof \app\components\Application))
+			return;
+
 		$this->templateColumns['_no'] = [
 			'header' => Yii::t('app', 'No'),
 			'class' => 'yii\grid\SerialColumn',
