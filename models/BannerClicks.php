7aa39223e3bb298b4ae98490cc65105f918beba0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-07-29 18:08:08 +0700

    modify getInfo with array column

--- models/BannerClicks.php
+++ models/BannerClicks.php
@@ -193,11 +193,13 @@ class BannerClicks extends \app\components\ActiveRecord
 	public static function getInfo($id, $column=null)
 	{
 		if($column != null) {
-			$model = self::find()
-				->select([$column])
-				->where(['click_id' => $id])
-				->one();
-			return $model->$column;
+			$model = self::find();
+			if(is_array($column))
+				$model->select($column);
+			else
+				$model->select([$column]);
+			$model = $model->where(['click_id' => $id])->one();
+			return is_array($column) ? $model : $model->$column;
 			
 		} else {
 			$model = self::findOne($id);
