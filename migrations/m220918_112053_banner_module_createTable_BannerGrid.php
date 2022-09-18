0ad53223e037c758a88adb9d567bae0a7b89a48d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2022-09-18 11:32:10 +0700

    migration: add bannerGridTable

---
+++ migrations/m220918_112053_banner_module_createTable_BannerGrid.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * m220918_112053_banner_module_createTable_BannerGrid
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 18 September 2022, 11:24 WIB
+ * @link https://github.com/ommu/mod-banner
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m220918_112053_banner_module_createTable_BannerGrid extends \yii\db\Migration
+{
+	public function up()
+	{
+		$tableOptions = null;
+		if ($this->db->driverName === 'mysql') {
+			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+		}
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_banner_grid';
+		if (!Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->createTable($tableName, [
+				'id' => Schema::TYPE_INTEGER . '(11) UNSIGNED NOT NULL',
+				'click' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+				'view' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+				'modified_date' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT \'trigger,on_update\'',
+				'PRIMARY KEY ([[id]])',
+				'CONSTRAINT ommu_banner_grid_ibfk_1 FOREIGN KEY ([[id]]) REFERENCES ommu_banners ([[banner_id]]) ON DELETE CASCADE ON UPDATE CASCADE',
+			], $tableOptions);
+		}
+	}
+
+	public function down()
+	{
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_banner_grid';
+		$this->dropTable($tableName);
+	}
+}
+
+
---
+++ migrations/m220918_112518_banner_module_addColumn_permanent_bannerGrid.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * m220918_112518_banner_module_addColumn_permanent_bannerGrid
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 18 September 2022, 11:26 WIB
+ * @link https://github.com/ommu/mod-banner
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m220918_112518_banner_module_addColumn_permanent_bannerGrid extends \yii\db\Migration
+{
+	public $tableName = 'ommu_banner_grid';
+
+	public function up()
+	{
+		$tableName = Yii::$app->db->tablePrefix . $this->tableName;
+		if (Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->addColumn(
+				$tableName,
+				'permanent',
+				$this->boolean()->notNull()->defaultValue(0)->after('id'),
+			);
+		}
+	}
+
+	public function down()
+	{
+		$tableName = Yii::$app->db->tablePrefix . $this->tableName;
+		if (Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->dropColumn($tableName, 'permanent');
+		}
+	}
+}
